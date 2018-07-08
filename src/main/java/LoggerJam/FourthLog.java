69f21b0f57e7e15ee7c3d5aff6fc8fd73519aff6
Martin Saitta <33335720+gigatron4624@users.noreply.github.com>
2018-07-08 09:26:01 -0400

    Update FourthLog.java

--- src/main/java/LoggerJam/FourthLog.java
+++ src/main/java/LoggerJam/FourthLog.java
@@ -1,4 +1,4 @@
-/** This code uses the SLF4J interface instead of the Logback interface. It otherwise issues the same logging calls
+/** This code uses the SLF4J interface only.  It otherwise issues the same logging calls
  * as ThirdLog did. SLF4J includes a logger level more critical than Error called Fatal. */
 
 package LoggerJam;
@@ -9,12 +9,21 @@ import org.slf4j.LoggerFactory;
 public class FourthLog {
     /* Format: Year-Month-Day Hour:Minute:Second:Milliseconds LEVEL [thread] class:line number - message */
 
-    static final Logger gimble = LoggerFactory.getLogger("FourthLog.class");
-
     public static void main(String[] args){
 
-        gimble.debug("Gunter glieben glauten globen!"); // print DEBUG message to console
-        gimble.info("Gunter glieben glauten globen!"); // print INFO message to console
-        gimble.error("Gunter glieben glauten globen!"); // print ERROR message to console and file
+        Logger gimble = LoggerFactory.getLogger("FourthLog.class");
+
+        if (gimble.isDebugEnabled()){
+            gimble.debug("Either the belt or motor is broken."); // print DEBUG message to console
+        }
+
+        if (gimble.isInfoEnabled()){
+            gimble.info("Check if the hose is clogged."); // print INFO message to console
+        }
+
+        if (gimble.isErrorEnabled()){
+            gimble.error("Washer won't spin the water out."); // print ERROR message to console and file
+        }
+
     }
 }
